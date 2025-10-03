368e8048172a90611d68eabed126a7af88b2bb46
Nisheel Nair <nisheeln1111@gmail.com>
2025-10-03 07:19:38 +0530

    first commit

---
+++ src/components/user/Profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>User Profile</h2>
+      <p>This is the user profile page.</p>
+    </div>
+  );
+};
+
+export default Profile;
